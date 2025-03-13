6625b5caa64202dea8efe65192dcf70eb3efda21
McKay Perry <hmperry17@gmail.com>
2025-03-12 22:39:51 -0600

    initial commit for code jam march 2025

---
+++ README.md
@@ -0,0 +1,21 @@
+# Code Jam 2025: Let's Make Earth Better
+
+### Description
+
+### Tech Stack
+
+The technologies listed below were used to create an application with responsive design.
+
+- HTML
+- CSS
+- Javascript
+
+### Deployment
+
+This webpage is deployed to GitHub pages:
+
+- Link: (https://hmperry.github.io/se_project_spots/)
+
+### Credits
+
+Illustration by Round Icons on Unsplash
---
+++ scripts/validation.js
@@ -0,0 +1,85 @@
+// const settings = {
+//   formSelector: ".modal__form",
+//   inputSelector: ".modal__input",
+//   submitButtonSelector: ".modal__submit-button",
+//   inactiveButtonClass: "modal__submit-button_inactive",
+//   inputErrorClass: "modal__input_type_error",
+//   errorClass: "modal__error",
+// };
+
+// const showInputError = (formEl, inputEl, errorMsg, config) => {
+//   const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+//   errorMsgEl.textContent = errorMsg;
+//   inputEl.classList.add(config.inputErrorClass);
+// };
+
+// const hideInputError = (formEl, inputEl, config) => {
+//   const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+//   errorMsgEl.textContent = "";
+//   inputEl.classList.remove(config.inputErrorClass);
+// };
+
+// const checkInputValidity = (formEl, inputEl, config) => {
+//   console.log(inputEl.validationMessage);
+
+//   if (!inputEl.validity.valid) {
+//     showInputError(formEl, inputEl, inputEl.validationMessage, config);
+//   } else {
+//     hideInputError(formEl, inputEl, config);
+//   }
+// };
+
+// const hasInvalidInput = (inputList, config) => {
+//   return inputList.some((inputEl) => {
+//     return !inputEl.validity.valid;
+//   });
+// };
+
+// const toggleButtonState = (inputList, buttonElement, config) => {
+//   if (hasInvalidInput(inputList)) {
+//     disableButton(buttonElement, config);
+//   } else {
+//     buttonElement.classList.remove(config.inactiveButtonClass);
+//     buttonElement.disabled = false;
+//   }
+// };
+
+// const disableButton = (buttonElement, config) => {
+//   buttonElement.classList.add(config.inactiveButtonClass);
+//   buttonElement.disabled = true;
+// };
+
+// const resetValidation = (formEl, inputList, config) => {
+//   inputList.forEach((input) => {
+//     hideInputError(formEl, input, config);
+//   });
+// };
+
+// const setEventListeners = (formEl, config) => {
+//   const inputList = Array.from(formEl.querySelectorAll(config.inputSelector));
+//   const buttonElement = formEl.querySelector(config.submitButtonSelector);
+
+//   console.log(inputList);
+//   console.log(buttonElement);
+
+//   toggleButtonState(inputList, buttonElement, config);
+//   formEl.addEventListener("reset", () => {
+//     disableButton(buttonElement, config);
+//   });
+
+//   inputList.forEach((inputEl) => {
+//     inputEl.addEventListener("input", function () {
+//       checkInputValidity(formEl, inputEl, config);
+//       toggleButtonState(inputList, buttonElement, config);
+//     });
+//   });
+// };
+
+// const enableValidation = (config) => {
+//   const formList = Array.from(document.querySelectorAll(config.formSelector));
+//   formList.forEach((formEl) => {
+//     setEventListeners(formEl, config);
+//   });
+// };
+
+// enableValidation(settings);
