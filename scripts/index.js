d429c93466632477bc1b177344c2970569d37181
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-03-14 21:05:09 -0500

    Feat: Add 10 questions for Energy Saver Quiz. Format as traditional HTML Form. Feat: display flex for Energy Saver Quiz form.

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,18 @@ PledgeWords.forEach((pledgeWord) => {
   span.style.color = `hsl(${Math.random() * 360}, 100%, 40%)`;
   wordCloud.appendChild(span);
 });
+
+/* Water Usage Questions:
+Enter Number - How many minutes do you typically spend in the shower?
+Yes, No, or Sometimes - Do you turn off the water while you brush your teeth? (Yes/No)
+Yes, No, or Sometimes - When washing dishes by hand, do you leave the water running? (Yes/No)
+Enter Number - How many times do you run the dishwasher per week?
+Enter Number - How often do you water your lawn or garden? (Every [blank] days)
+
+Electricity Usage Questions:
+Yes, No, or Sometimes - Do you turn off the lights when you leave a room? (Yes/No/Sometimes)
+Yes, No, or Sometimes - Do you unplug electronics when they are not in use? (Yes/No/Sometimes)
+Yes, No, or Sometimes - Do you use a ceiling fan instead of air conditioning when possible? (Yes/No/Sometimes)
+Yes, No, or Sometimes - Do you charge your phone overnight? (Yes/No)
+Yes, No, or Sometimes - Do you use a programmable thermostat to manage heating and cooling? (Yes/No/Not Sure)
+*/
