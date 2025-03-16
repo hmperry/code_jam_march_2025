a238973a8eb69d3ce8aae5b516c9a33a735e9db5
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-03-16 12:25:45 -0500

    Feat: Add functionality for Quiz. Update Quiz Input values to use in functionality.
    Fix: Move questions in JS comment to Quiz functionality section of file.
    
    Fix: Change quiz__Checkboxes class to quiz__radio_bottons class to betern describe.
    
    Feat: Add quiz result modal to html to open when quiz submit button is clicked.
    
    Fix: Edit closeModal and openModal functions to allow use on both modals.

--- scripts/index.js
+++ scripts/index.js
@@ -46,20 +46,6 @@ PledgeWords.forEach((pledgeWord) => {
   wordCloud.appendChild(span);
 });
 
-/* Water Usage Questions:
-Enter Number - How many minutes do you typically spend in the shower?
-Yes, No, or Sometimes - Do you turn off the water while you brush your teeth? (Yes/No)
-Yes, No, or Sometimes - When washing dishes by hand, do you leave the water running? (Yes/No)
-Enter Number - How many times do you run the dishwasher per week?
-Enter Number - How often do you water your lawn or garden? (Every [blank] days)
-
-Electricity Usage Questions:
-Yes, No, or Sometimes - Do you turn off the lights when you leave a room? (Yes/No/Sometimes)
-Yes, No, or Sometimes - Do you unplug electronics when they are not in use? (Yes/No/Sometimes)
-Yes, No, or Sometimes - Do you use a ceiling fan instead of air conditioning when possible? (Yes/No/Sometimes)
-Yes, No, or Sometimes - Do you charge your phone overnight? (Yes/No)
-Yes, No, or Sometimes - Do you use a programmable thermostat to manage heating and cooling? (Yes/No/Not Sure)
-*/
 //Popup Pledge Window
 
 //Pledge Button in the Nav
@@ -69,28 +55,31 @@ const pledgeButton = document.querySelector(".nav-button__pledge");
 const pledgeModal = document.querySelector("#add-pledge-modal");
 
 //Close Button in the Modal
-const closeButton = document.querySelector(".modal__close-button");
+const closeButtons = document.querySelectorAll(".modal__close-button");
 
 //Event Listener on Click of pledgeButton
 pledgeButton.addEventListener("click", () => {
-  openModal();
+  openModal(pledgeModal);
 });
 
 //Open Modal Function
-function openModal() {
+//Edited to open for both modals. - Roman
+function openModal(modal) {
   console.log("Is this correct?");
-  pledgeModal.classList.add("modal_opened");
+  modal.classList.add("modal_opened");
 }
 
-//Event Listener on Click of closeButton
-closeButton.addEventListener("click", () => {
-  closeModal();
+//Event Listener on Click of closeButtons
+//Edited to close for both modal close buttons. - Roman
+closeButtons.forEach((button) => {
+  const popup = button.closest(".modal");
+  button.addEventListener("click", () => closeModal(popup));
 });
 
 //Close Modal Function
-function closeModal() {
+function closeModal(modal) {
   console.log("Close function shows up?");
-  pledgeModal.classList.remove("modal_opened");
+  modal.classList.remove("modal_opened");
 }
 
 // Conditional Visibility of Email Field
@@ -107,3 +96,170 @@ yesEmail.addEventListener("change", () => {
 noEmail.addEventListener("change", () => {
   emailInputElement.style.display = "none";
 });
+
+/* ========================================= Energy Saver Quiz ========================================= */
+/* Water Usage Questions:
+Enter Number - How many minutes do you typically spend in the shower?
+Yes, No, or Sometimes - Do you turn off the water while you brush your teeth? (Yes/No)
+Yes, No, or Sometimes - When washing dishes by hand, do you leave the water running? (Yes/No)
+Enter Number - How many times do you run the dishwasher per week?
+Enter Number - How often do you water your lawn or garden? (Every [blank] days)
+
+Electricity Usage Questions:
+Yes, No, or Sometimes - Do you turn off the lights when you leave a room? (Yes/No/Sometimes)
+Yes, No, or Sometimes - Do you unplug electronics when they are not in use? (Yes/No/Sometimes)
+Yes, No, or Sometimes - Do you use a ceiling fan instead of air conditioning when possible? (Yes/No/Sometimes)
+Yes, No, or Sometimes - Do you charge your phone overnight? (Yes/No)
+Yes, No, or Sometimes - Do you use a programmable thermostat to manage heating and cooling? (Yes/No/Not Sure)
+*/
+
+const radioBtnQuestions = [
+  // {
+  //   name: "shower-minutes",
+  //   energyType: "gallons",
+  // },
+  {
+    name: "brush-teeth",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "gallons",
+  },
+  {
+    name: "hand-wash",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "gallons",
+  },
+  // {
+  //   name: "dishwasher-cycles",
+  //   energyType: "gallons",
+  // },
+  // {
+  //   name: "lawn-watering",
+  //   energyType: "gallons",
+  // },
+  {
+    name: "lights-off",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "watts",
+  },
+  {
+    name: "unplug-electronics",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "watts",
+  },
+  {
+    name: "ceiling-fan",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "watts",
+  },
+  {
+    name: "charge-phone",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "watts",
+  },
+  {
+    name: "thermostat",
+    choice: ["yes", "sometimes", "no"],
+    energyType: "watts",
+  },
+];
+
+const quiz = document.querySelector(".quiz__Questions");
+const numAnswers = quiz.querySelectorAll(".quiz__Question_number_Answer");
+const radioBtns = quiz.querySelectorAll(".quiz__radio_buttons");
+const choiceAnswers = quiz.querySelectorAll(".quiz__Question_choice_Answer");
+let userGallons = 0;
+let userWatts = 0;
+
+function calculateNumAnswers(event) {
+  userGallons = 0;
+  userWatts = 0;
+  event.preventDefault();
+  numAnswers.forEach((numAnswers) => {
+    if (numAnswers.id == "shower-minutes") {
+      userGallons += Math.round(10 - ((numAnswers.value - 5) / 15) * 10);
+      // console.log(`${numAnswers.id} added to userGallons: ${userGallons}`);
+    } else if (numAnswers.id == "dishwasher-cycles") {
+      userGallons += 7 - Number(numAnswers.value) + 3;
+      // console.log(`${numAnswers.id} added to userGallons: ${userGallons}`);
+    } else if (numAnswers.id == "lawn-watering") {
+      //10 gallons for 10 minutes
+      userGallons += 7 - Number(numAnswers.value) + 3;
+      // console.log(`${numAnswers.id} added to userGallons: ${userGallons}`);
+    }
+  });
+  // console.log(userGallons);
+  // return userGallons;
+}
+
+function calculateChoiceAnswers() {
+  choiceAnswers.forEach((element) => {
+    const elementName = element.name;
+    // console.log(element);
+    // console.log(element.checked);
+    // console.log(element.name);
+
+    // loop through radioBtnQuestions to compare the name within it to the element.name
+    for (let i = 0; i < radioBtnQuestions.length; i++) {
+      if (radioBtnQuestions[i].name === elementName && element.checked) {
+        // check which energy type is used for the element, and update the appropriate energy variable
+        // console.log(`radioBtn Name is ${radioBtnQuestions[i].name}`);
+        if (radioBtnQuestions[i].energyType === "gallons") {
+          // console.log(`Value to add to Gallons: ${Number(element.value)}`);
+          userGallons += Number(element.value);
+        } else if (radioBtnQuestions[i].energyType === "watts") {
+          // console.log(`Value to add to Watts: ${Number(element.value)}`);
+          userWatts += Number(element.value);
+        }
+        break;
+      }
+    }
+  });
+  // console.log(`userGallons: ${userGallons}`);
+  // console.log(`userWatts: ${userWatts}`);
+}
+
+function userScore() {
+  const archetypeOne = "Earth's Best Ally";
+  const archetypeTwo = "Tree Hugger ";
+  const archetypeThree = "Sustainable Striver";
+  const archetypeFour = "Resource Reckless";
+  const userScoreNum = userGallons + userWatts;
+  console.log(userScoreNum);
+  switch (true) {
+    case userScoreNum >= 0 && userScoreNum <= 25:
+      console.log(archetypeFour);
+      break;
+    case userScoreNum >= 26 && userScoreNum <= 50:
+      console.log(archetypeThree);
+      break;
+    case userScoreNum >= 51 && userScoreNum <= 75:
+      console.log(archetypeTwo);
+      break;
+    case userScoreNum >= 76 && userScoreNum <= 100:
+      console.log(archetypeOne);
+      break;
+    default:
+      console.log(
+        `Hmmm... something is wrong here. Please check the quiz and ensure you answered all questions.`
+      );
+  }
+}
+
+quiz.addEventListener("submit", (data) => {
+  calculateNumAnswers(data);
+  calculateChoiceAnswers(data);
+  userScore(data);
+  openModal(quizResultsModal);
+});
+
+/* TO DO
+-- DONE Set up SUBMIT BUTTON
+-- DONE Create formula for the numerical answer questions.
+-- DONE Create formula for the radio button questions.
+-- UNSURE Can all of the usage formulas be used in one function?
+-- DONE When Submit button is submitted, ensure execution of all usage formulas.
+-- Create modal to project % score to user after submission.
+*/
+
+/* ========================================= Quiz Results Modal ========================================= */
+const quizResultsModal = document.querySelector("#quiz-results-modal");
